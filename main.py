a822488f7e0b93772328d2696ed74d23c10a36ce
Prem Udeshi <premudeshi99@gmail.com>
2020-08-23 01:15:30 +0300

    Code working

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-import publicip
 from twilio.rest import Client
 from config import *
+from requests import get
+
+ip = get('https://api.ipify.org').text
 
 
-print(sid)
+#print(sid)
 
-print(publicip.get())
+text = 'Your IP is ' + str(ip)
 
-file = open('publicip.txt', 'w+')
+account_sid = sid
+auth_token = auth_token
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    from_=froms,
+    body=text,
+    to=tos
+)
+
+print(message.sid)
+
+
+#file = open('publicip.txt', 'w+')
 
